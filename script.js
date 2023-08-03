1167c163b65263d1da8976af847feee954762c73
fokhrul2029 <ifokhrul.2029@gmail.com>
2023-08-04 00:32:33 +0600

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-function deleteData(){
-    document.getElementById("result").value = "" ; 
+// Dark and Light mode functionalities
+// Element Selector
+const mode = document.querySelector(".mode");
+mode.addEventListener("click", () => {
+  const table = document.querySelector("table");
+  const tbody = document.querySelector("tbody");
+  const title = document.querySelector(".title");
+  const currentMode = "Light";
+  if (mode.textContent === currentMode) {
+    mode.textContent = "Dark";
+    title.style.color = "var(--dark)";
+    table.style.background = "var(--white)";
+    tbody.style.background = "var(--light)";
+  } else {
+    mode.textContent = currentMode;
+    title.style.color = "";
+    table.style.background = "";
+    tbody.style.background = "";
+  }
+});
+// Calculator Functionality
+const deleteData = document.querySelector(".delete");
+const display = document.querySelector(".display");
+const results = document.querySelector(".answer");
+
+function calcolate(value) {
+  display.innerText += value;
 }
-function calculator(newData){
-    document.getElementById("result").value += newData ;
-}
-function Result(){
-    let i = document.getElementById("result").value ; 
-    i = eval(i) ; 
-    document.getElementById("result").value = i ;
-}
+deleteData.onclick = () => {
+  display.innerText = "";
+};
+results.onclick = () => {
+  let result = display.innerText;
+  result = eval(result);
+  display.innerText = result;
+};
