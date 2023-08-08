b4e39ba1198c4dabf2cc2b8187b3b805a6485196
fokhrul2029 <ifokhrul.2029@gmail.com>
2023-08-08 23:53:15 +0600

    first commit

--- script.js
+++ script.js
@@ -5,17 +5,23 @@ mode.addEventListener("click", () => {
   const table = document.querySelector("table");
   const tbody = document.querySelector("tbody");
   const title = document.querySelector(".title");
+  const body = document.querySelector("body")
+  const result = document.querySelector(".result")
   const currentMode = "Light";
   if (mode.textContent === currentMode) {
     mode.textContent = "Dark";
     title.style.color = "var(--dark)";
     table.style.background = "var(--white)";
     tbody.style.background = "var(--light)";
+    body.style.background = "var(--light)"
+    result.style.color = "var(--dark)"
   } else {
     mode.textContent = currentMode;
     title.style.color = "";
     table.style.background = "";
     tbody.style.background = "";
+    body.style.background = "";
+    result.style.color = ""
   }
 });
 // Calculator Functionality
